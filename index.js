d855c7506b9995fc441fcce204e46986b38719b5
Gokulraj0606 <gokul222.gr@gmail.com>
2024-02-15 15:22:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require("./app")
+const path = require("path")
+const connectDatabase = require("./config/database")
+
+
+connectDatabase()
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is listening to the port : ${process.env.PORT}`)
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`)
+    console.log("shutting down the server due to unhandle rejection error")
+    server.close(() => {
+        process.exit(1)
+    })
+})
+
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`)
+    console.log("shutting down the server due to uncaught exception rejection error")
+    server.close(() => {
+        process.exit(1)
+    })
+})
